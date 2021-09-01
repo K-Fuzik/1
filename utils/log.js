309154dbf8c2b96a9cfef83e1130a5784c837c85
K-Fuzik <89893109+K-Fuzik@users.noreply.github.com>
2021-09-01 20:33:02 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -3,13 +3,13 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ ] » ') + data);
+			console.log(chalk.yellow('[ ❕ ] ❯ ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ ] » ') + data);
+			console.log(chalk.red('[ ❕ ] ❯ ') + data);
 			break;
 		default:
-			console.log(chalk.magenta(`${option} » `) + data);
+			console.log(chalk.magenta(`${option} ❯ `) + data);
 			break;
 	}
 }
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ LOADER ] » ') + data);
+			console.log(chalk.yellow('[ LOADER ] ❯  ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ LOADER ] » ') + data);
+			console.log(chalk.red('[ LOADER ] ❯  ') + data);
 			break;
 		default:
-			console.log(chalk.green(`[ LOADER ] » `) + data);
+			console.log(chalk.green(`[ LOADER ] ❯  `) + data);
 			break;
 	}
 }
